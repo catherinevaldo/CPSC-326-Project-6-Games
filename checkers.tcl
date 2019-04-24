5b4c4c992397f2fcf71efd5dc11be1bc2bf868ee
Elizabeth Larson <49002464+eml2017@users.noreply.github.com>
2019-04-24 16:00:52 -0700

    Update checkers.tcl
    it done

--- checkers.tcl
+++ checkers.tcl
@@ -1,87 +1,49 @@
-# Checkers
+# eightball.tcl
+# Katie Wraith, Caterina Valdovinos, and Elizabeth Larson
+# Simple magic eight ball simulation
+# Prompts user input and displays an answer to their question upon pressing a button
 
-set xcor 50
-set ycor 50
-canvas .checkerBoard -background black -width 400 -height 400 
-pack .checkerBoard
+wm title . "8-Ball"
+frame .eightBall -padx 5 -pady 5
+pack .eightBall -fill both -expand 1
 
-#copied from internet... fix!
-.checkerBoard bind all <1> {set c(X) [.checkerBoard canvasx %x]; set c(Y) [.checkerBoard canvasy %y]}
-.checkerBoard bind mv <B1-Motion> {mv %x %y}
-.checkerBoard bind mv <ButtonRelease-1> {drop [.checkerBoard find withtag current]}
-
-proc makeSquare {x y args} {
-  global xcor ycor
-  .checkerBoard create rectangle $x $y [expr {$x+$xcor}] [expr {$y+$ycor}] -fill red -tags $args
-}
-
-proc makeP1Piece {x y args} {
-  global xcor ycor
-  .checkerBoard create oval [expr {$x+2}] [expr {$y+2}] [expr {$x+$xcor-3}] [expr {$y+$ycor-3}] -fill white -tags [concat $args mv]
-}
-
-proc makeP2Piece {x y color args} {
-  global xcor ycor
-  .checkerBoard create oval [expr {$x+2}] [expr {$y+2}] [expr {$x+$xcor-3}] [expr {$y+$ycor-3}] -fill green -tags [concat $args mv]
-}
-
-proc p1Jumped {} {
-  if {$ycor}
-}
-
-#copied from internet... fix!
-proc mv {x y} {
-  global c
-  set x  [.checkerBoard canvasx $x]
-  set y  [.checkerBoard canvasy $y]
-  set id [.checkerBoard find withtag current]
-  .checkerBoard move $id [expr {$x-$c(X)}] [expr {$y-$c(Y)}]
-  .checkerBoard raise $id
-  set c(X) $x; set c(Y) $y
-}
- 
-#copied from internet... fix!
-proc drop {id} {
-  global c xcor ycor
-  set cx0 [expr {int($c(X) / $xcor) * $xcor + 2}]
-  set cy0 [expr {int($c(Y) / $ycor) * $ycor + 2}]
-  set cx1 [expr {$cx0 + $xcor - 3}]
-  set cy1 [expr {$cy0 + $ycor - 3}]
-  .checkerBoard coords $id $cx0 $cy0 $cx1 $cy1        
-}
-
-proc setUpBoard {} {
-  global xcor ycor
-  for {set i 0; set y 0} {$i < 8} {incr i; incr y $ycor} {
-   for {set j 0; set x 0} {$j < 8} {incr j; incr x $xcor} {
-     if {$i % 2 == 0 && $j % 2 == 0} {
-       makeSquare $x $y red
-     }
-     if {$i % 2 == 1 && $j % 2 == 1} {
-       makeSquare $x $y
-     }
-     if {$i % 2 == 0 && $j % 2 == 1} {
-       if {$i < 3} {
-	     makeP1Piece $x $y white player1
-       }
-       if {$i >= 5} {
-         makeP2Piece $x $y green player2
-       }
-     }
-     if {$i % 2 == 1 && $j % 2 == 0} {
-	   if {$i < 3} {
-	     makeP1Piece $x $y white player1
-       }
-       if {$i >= 5} {
-         makeP2Piece $x $y green player2
-       }
-     }
-    }
-  }
-}
-
+# Sets up the user input field + button that triggers the ball to "shake"
 proc playGame {} {
+  grid [ label .eightBall.welcome -text "Got a question? Ask the magic eight ball!" ]
+  grid [ button .eightBall.shakeBall -text "Shake!" -command shakeBall ] -column 4 -row 1 -sticky w -columnspan 2
+  set gr1c0 [ entry .eightBall.r1c0 -textvariable r1c0 -width 30 -state normal ]
+  grid $gr1c0 -column 0 -row 1
 }
 
-setUpBoard
+# Randomly selects one of the 20 answers
+proc shakeBall {} {
+  set randomNumber [expr {int(rand()*20)}]
+  
+  # Affirmative answers
+  if {$randomNumber == 1} { grid [ label .eightBall.answer -text "It is certain." ] }
+  if {$randomNumber == 2} { grid [ label .eightBall.answer -text "It is decidedly so." ] }
+  if {$randomNumber == 3} { grid [ label .eightBall.answer -text "Without a doubt." ] }
+  if {$randomNumber == 4} { grid [ label .eightBall.answer -text "Yes - definitely." ] }
+  if {$randomNumber == 5} { grid [ label .eightBall.answer -text "You may rely on it." ] }
+  if {$randomNumber == 6} { grid [ label .eightBall.answer -text "As I see it, yes." ] }
+  if {$randomNumber == 7} { grid [ label .eightBall.answer -text "Most likely." ] }
+  if {$randomNumber == 8} { grid [ label .eightBall.answer -text "Outlook good." ]}
+  if {$randomNumber == 9} { grid [ label .eightBall.answer -text "Yes." ] }
+  if {$randomNumber == 10} { grid [ label .eightBall.answer -text "Signs point to yes." ] }
+  
+  # Non-committal answers
+  if {$randomNumber == 11} { grid [ label .eightBall.answer -text "Reply hazy, try again." ] }
+  if {$randomNumber == 12} { grid [ label .eightBall.answer -text "Ask again later." ] }
+  if {$randomNumber == 13} { grid [ label .eightBall.answer -text "Better not tell you now." ] }
+  if {$randomNumber == 14} { grid [ label .eightBall.answer -text "Cannot predict now." ] }
+  if {$randomNumber == 15} { grid [ label .eightBall.answer -text "Concentrate and ask again." ] }
+
+  # Negative answers
+  if {$randomNumber == 16} { grid [ label .eightBall.answer -text "Don't count on it." ] }
+  if {$randomNumber == 17} { grid [ label .eightBall.answer -text "My reply is no." ] }
+  if {$randomNumber == 18} { grid [ label .eightBall.answer -text "My sources say no." ] }
+  if {$randomNumber == 19} { grid [ label .eightBall.answer -text "Outlook not so good." ] }
+  if {$randomNumber == 20} { grid [ label .eightBall.answer -text "Very doubtful." ] }
+}
+
 playGame
