8ab713794eda392375afd880b22d065e3ab0e4a6
Elizabeth Larson <49002464+eml2017@users.noreply.github.com>
2019-04-24 18:34:00 -0700

    Update eightball.tcl

--- eightball.tcl
+++ eightball.tcl
@@ -1,56 +1,49 @@
-# 8ball game
+# eightball.tcl
+# Katie Wraith, Caterina Valdovinos, and Elizabeth Larson
+# Simple magic eight ball simulation
+# Prompts user input and displays an answer to their question upon pressing a button
 
-destroy  .buttons
-toplevel .buttons
-wm title .buttons "8-ball game"
-set count 0
+wm title . "8-Ball"
+frame .eightBall -padx 5 -pady 5
+pack .eightBall -fill both -expand 1
 
-proc add_frame title {
-  global frame count
-  set frame .buttons.frame$count
-  frame $frame -border 2 -relief groove
-  label $frame.label -text $title
-  pack  $frame       -side left -padx 2 -pady 2 -anchor n -fill both
-  pack  $frame.label -side top  -padx 2 -pady 2
-  incr count
+# Sets up the user input field + button that triggers the ball to "shake"
+proc playGame {} {
+  grid [ label .eightBall.welcome -text "Got a question? Ask the magic eight ball!" ]
+  grid [ button .eightBall.shakeBall -text "Shake!" -command shakeBall ] -column 4 -row 1 -sticky w -columnspan 2
+  set gr1c0 [ entry .eightBall.r1c0 -textvariable r1c0 -width 30 -state normal ]
+  grid $gr1c0 -column 0 -row 1
 }
 
-proc add_button { title command } {
-  global frame count
-  button $frame.$count -text $title -command $command
-  pack   $frame.$count -side top -pady 1 -padx 1 -fill x
-  incr count
-}
-
+# Randomly selects one of the 20 answers
 proc shakeBall {} {
   set randomNumber [expr {int(rand()*20)}]
   
   # Affirmative answers
-  if {$randomNumber == 1} { add_frame "It is certain." }
-  if {$randomNumber == 2} { add_frame "It is decidedly so." }
-  if {$randomNumber == 3} { add_frame "Without a doubt." }
-  if {$randomNumber == 4} { add_frame "Yes - definitely." }
-  if {$randomNumber == 5} { add_frame "You may rely on it." }
-  if {$randomNumber == 6} { add_frame "As I see it, yes." }
-  if {$randomNumber == 7} { add_frame "Most likely." }
-  if {$randomNumber == 8} { add_frame "Outlook good." }
-  if {$randomNumber == 9} { add_frame "Yes." }
-  if {$randomNumber == 10} { add_frame "Signs point to yes." }
+  if {$randomNumber == 1} { grid [ label .eightBall.answer -text "It is certain." ] }
+  if {$randomNumber == 2} { grid [ label .eightBall.answer -text "It is decidedly so." ] }
+  if {$randomNumber == 3} { grid [ label .eightBall.answer -text "Without a doubt." ] }
+  if {$randomNumber == 4} { grid [ label .eightBall.answer -text "Yes - definitely." ] }
+  if {$randomNumber == 5} { grid [ label .eightBall.answer -text "You may rely on it." ] }
+  if {$randomNumber == 6} { grid [ label .eightBall.answer -text "As I see it, yes." ] }
+  if {$randomNumber == 7} { grid [ label .eightBall.answer -text "Most likely." ] }
+  if {$randomNumber == 8} { grid [ label .eightBall.answer -text "Outlook good." ]}
+  if {$randomNumber == 9} { grid [ label .eightBall.answer -text "Yes." ] }
+  if {$randomNumber == 10} { grid [ label .eightBall.answer -text "Signs point to yes." ] }
   
   # Non-committal answers
-  if {$randomNumber == 11} { add_frame "Reply hazy, try again." }
-  if {$randomNumber == 12} { add_frame "Ask again later." }
-  if {$randomNumber == 13} { add_frame "Better not tell you now." }
-  if {$randomNumber == 14} { add_frame "Cannot predict now." }
-  if {$randomNumber == 15} { add_frame "Concentrate and ask again." }
+  if {$randomNumber == 11} { grid [ label .eightBall.answer -text "Reply hazy, try again." ] }
+  if {$randomNumber == 12} { grid [ label .eightBall.answer -text "Ask again later." ] }
+  if {$randomNumber == 13} { grid [ label .eightBall.answer -text "Better not tell you now." ] }
+  if {$randomNumber == 14} { grid [ label .eightBall.answer -text "Cannot predict now." ] }
+  if {$randomNumber == 15} { grid [ label .eightBall.answer -text "Concentrate and ask again." ] }
 
   # Negative answers
-  if {$randomNumber == 16} { add_frame "Don't count on it." }
-  if {$randomNumber == 17} { add_frame "My reply is no." }
-  if {$randomNumber == 18} { add_frame "My sources say no." }
-  if {$randomNumber == 19} { add_frame "Outlook not so good." }
-  if {$randomNumber == 20} { add_frame "Very doubtful." }
+  if {$randomNumber == 16} { grid [ label .eightBall.answer -text "Don't count on it." ] }
+  if {$randomNumber == 17} { grid [ label .eightBall.answer -text "My reply is no." ] }
+  if {$randomNumber == 18} { grid [ label .eightBall.answer -text "My sources say no." ] }
+  if {$randomNumber == 19} { grid [ label .eightBall.answer -text "Outlook not so good." ] }
+  if {$randomNumber == 20} { grid [ label .eightBall.answer -text "Very doubtful." ] }
 }
 
-add_frame  "What's your question?"
-add_button "Shake the 8-ball"     { shakeBall }
+playGame
